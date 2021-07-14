9c9ecb407f518a840cc8ec672aa1ddad596b18f1
Camila Martinez Bedoya <camilamarb98@gmail.com>
2021-07-13 20:34:21 -0500

    Change Security settings

--- src/main/java/pe/com/casatranslima/security/SecurityConfiguration.java
+++ src/main/java/pe/com/casatranslima/security/SecurityConfiguration.java
@@ -40,7 +40,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/applications","/registration", "/js/**", "/css/**", "/img/**").permitAll().anyRequest()
+        // http.authorizeRequests().antMatchers("/home", "/applications","/registration", "/js/**", "/css/**", "/img/**").permitAll().anyRequest()
+        //         .authenticated().and().formLogin().loginPage("/login").permitAll().and().logout()
+        //         .invalidateHttpSession(true).clearAuthentication(true)
+        //         .logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login?logout")
+        //         .permitAll();
+
+                http.authorizeRequests()
+                .antMatchers("/**").permitAll().anyRequest()
                 .authenticated().and().formLogin().loginPage("/login").permitAll().and().logout()
                 .invalidateHttpSession(true).clearAuthentication(true)
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login?logout")
