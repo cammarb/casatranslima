9b2510ca5fda40e0dc2b2290ea442313e9ae87b3
Camila Martinez Bedoya <camilamarb98@gmail.com>
2021-07-14 01:05:47 -0500

    Edit Secutiry Config file

--- src/main/java/pe/com/casatranslima/security/SecurityConfiguration.java
+++ src/main/java/pe/com/casatranslima/security/SecurityConfiguration.java
@@ -47,7 +47,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         //         .permitAll();
 
                 http.authorizeRequests()
-                .antMatchers("/**").permitAll().anyRequest()
+                .antMatchers("/","/applications/**", "/js/**", "/css/**", "/img/**").permitAll().anyRequest()
                 .authenticated().and().formLogin().loginPage("/login").permitAll().and().logout()
                 .invalidateHttpSession(true).clearAuthentication(true)
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login?logout")
