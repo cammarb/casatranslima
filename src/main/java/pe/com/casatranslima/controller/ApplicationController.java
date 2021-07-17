50ed77e7837e502e3c9e6fa934ac35ba76a17bd8
Camila Martinez Bedoya <camilamarb98@gmail.com>
2021-07-16 21:31:11 -0500

    Clean

--- src/main/java/pe/com/casatranslima/controller/ApplicationController.java
+++ src/main/java/pe/com/casatranslima/controller/ApplicationController.java
@@ -1,6 +1,5 @@
 package pe.com.casatranslima.controller;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
--- src/main/java/pe/com/casatranslima/service/crud/HouseServiceImpl.java
+++ src/main/java/pe/com/casatranslima/service/crud/HouseServiceImpl.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import pe.com.casatranslima.model.entity.House;
 import pe.com.casatranslima.model.repository.HouseRepository;
